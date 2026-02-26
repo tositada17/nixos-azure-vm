527a8b9d4f3dd846836ec1f56018f72b14b8e67f
tositada17 <tositada17@gmail.com>
2026-02-26 10:50:49 +0000

    work vm

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,12 +1,35 @@
-{modulesPath, ...}:
+{ modulesPath ,pkgs , ... }:
 
 {
-  import = [
-    "${modulesPath}/virtualisation/azure-common.nix"
-    "${modulesPath}/virtualisation/azure-image.nix"
-  ]
+  # To build the configuration or use nix-env, you need to run
+  # either nixos-rebuild --upgrade or nix-channel --update
+  # to fetch the nixos channel.
 
+  # This configures everything but bootstrap services,
+  # which only need to be run once and have already finished
+  # if you are able to see this comment.
+  imports = [
+   "${modulesPath}/virtualisation/azure-common.nix"
+   "${modulesPath}/virtualisation/azure-image.nix"
+  ];
+  
+  
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    gh
+    curl
+    wget
+  ];
+  
+  users.users."nixos" = {
+    isNormalUser = true;
+    home = "/home/nixos";
+    extraGroups = [ "wheel docker" ]; # Enable ‘sudo’ for the user.
+  };
+  #nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  virtualisation.azure-image.vmGeneration = "v2"
+  # Please set the VM Generation to the actual value
+  virtualisation.azureImage.vmGeneration = "v2";
 }
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,13 +2,14 @@
   description = "flake ";
 
   inputs = {
+    # NixOS official package source, here using the nixos-25.05 branch
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
     # The host with the hostname `nixos` will use this configuration
     nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
+      nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
